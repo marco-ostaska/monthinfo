d6a0e23023ea7c0583f56ceb0a4018470fd4883b
Marco Ostaska <marcoan@kyndryl.com>
2022-12-23 17:21:33 -0300

    refactor

--- monthinfo/monthinfo.py
+++ monthinfo/monthinfo.py
@@ -5,7 +5,7 @@ from enum import Enum
 from tkinter import Y
 
 
-class WEEK:
+class Week:
     first_day = calendar.MONDAY
     days = {0: "Monday", 1: "Tuesday", 2: "Wednesday",
             3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
@@ -15,90 +15,15 @@ class WEEK:
         return {value: key for key, value in cls.days.items()}
 
 
-def setfirstweekday(first_week_day: str):
-    WEEK.first_day = WEEK.inverted_days()[first_week_day.capitalize()]
-    calendar.setfirstweekday(WEEK.first_day)
-
-@dataclass()
-class Validators:
-
 @dataclass()
 class Month:
     year: int
     month_number: int
 
 
-    @property
-    def calendar(self):
-        """
-        Returns a matrix representing a month's calendar.
-
-        Each row represents a week, and each column represents a day of the week.
-        Days that are not part of the current month are represented by zeros.
-
-        Returns:
-            A list of lists containing integers representing the days of the month.
-        """
-        return calendar.monthcalendar(self.year, self.month_number)
-
-    @property
-    def list_of_days(self) -> list:
-        """
-        Returns a list of the days in the current month.
-
-        The list contains integers representing the days of the month, starting from 1.
-
-        Returns:
-            A list of integers representing the days of the month.
-        """
-        return list(range(1, calendar.monthrange(self.year, self.month_number)[1]+1))
-
-    @property
-    def list_of_weeks(self) -> list:
-        """
-        Returns a list of the weeks in the current month.
-
-        Each element of the list is a list of integers representing the days of the week.
-        Days that are not part of the current month are represented by zeros.
-
-        Returns:
-            A list of lists containing integers representing the days of the month.
-        """
-        return list(calendar.monthcalendar(self.year, self.month_number))
-
-    @property
-    def number_of_weeks(self) -> int:
-        """
-        Returns the number of weeks in the current month.
-
-        Returns:
-            An integer representing the number of weeks in the current month.
-        """
-        return len(calendar.monthcalendar(self.year, self.month_number))
-
-    @property
-    def number_of_days(self) -> int:
-        """
-        Returns the number of days in the current month.
-
-        Returns:
-            An integer representing the number of days in the current month.
-        """
-        return calendar.monthrange(self.year, self.month_number)[1]
-
 @dataclass()
-class DayOfWeek:
+class Validators:
     month: Month
-
-    @property
-    def first_week_day(self) -> str:
-        """
-        Returns the day of the week of the first day of the current month.
-
-        Returns:
-            A string representing the day of the week (e.g. "Monday").
-        """
-        return WEEK.days[datetime.date(self.month.year, self.month.month_number, 1).weekday()]
 
     def is_day_in_weekday(self, day: int, weekday: str) -> bool:
         """
@@ -111,9 +36,9 @@ class DayOfWeek:
         Returns:
             bool: True if the day falls on the given weekday, False otherwise.
         """
-        return datetime.date(self.month.year, self.month.month_number, day).weekday() == WEEK.inverted_days()[weekday.capitalize()]
+        return datetime.date(self.month.year, self.month.month_number, day).weekday() == Week.inverted_days()[weekday.capitalize()]
 
-    def is_in_first_weekday(self, day: int, weekday:str) -> bool:
+    def is_in_first_weekday(self, day: int, weekday: str) -> bool:
         """
         Returns True if the specified day is the first Weekday of the month, False otherwise.
 
@@ -123,40 +48,7 @@ class DayOfWeek:
         Returns:
             A boolean indicating whether the specified day is the first Weekday of the month.
         """
-        return self.is_day_in_weekday(day, weekday.capitalize()) and day < 8
-
-    def list_of_weekday(self, weekday: str) -> list:
-        """
-        Returns a list of days in the current month that are given weekday.
-
-        Returns:
-            A list of integers representing the days of the month that are weekday.
-
-        Example:
-            If the current month is December 2022, the method would return [5, 12, 19, 26] for Mondays.
-        """
-        return [day for day in self.month.list_of_days if self.is_day_in_weekday(day, weekday.capitalize())]
-
-    def number_of_weekday(self, weekday: str) -> int:
-        """
-        Returns the number of weekday in the current month.
-
-        Returns:
-            An integer representing the number of weekday in the current month.
-        """
-        return len(self.list_of_weekday(weekday.capitalize()))
-
-    @property
-    def number_of_weekends(self) -> float:
-        """
-        Returns the number of weekend days (Saturdays and Sundays) in the current month.
-
-        The number of weekend days is calculated as the maximum number of Saturdays or Sundays in the month.
-
-        Returns:
-            An float representing the number of weekend days in the current month.
-        """
-        return (self.number_of_weekday("saturday") + self.number_of_weekday("sunday")) * 0.5
+        return self.is_day_in_weekday(day, weekday) and day < 8
 
     def is_weekend(self, day) -> bool:
         """
@@ -173,6 +65,102 @@ class DayOfWeek:
         """
         return datetime.date(self.month.year, self.month.month_number, day).weekday() >= 5
 
+
+@dataclass()
+class Retrievers:
+    month: Month
+    validator: Validators
+
+    @property
+    def list_of_days(self) -> list:
+        """
+        Returns a list of the days in the current month.
+
+        The list contains integers representing the days of the month, starting from 1.
+
+        Returns:
+            A list of integers representing the days of the month.
+        """
+        return list(range(1, calendar.monthrange(self.month.year, self.month.month_number)[1]+1))
+
+    @property
+    def list_of_weeks(self) -> list:
+        """
+        Returns a list of the weeks in the current month.
+
+        Each element of the list is a list of integers representing the days of the Week.
+        Days that are not part of the current month are represented by zeros.
+
+        Returns:
+            A list of lists containing integers representing the days of the month.
+        """
+        return list(calendar.monthcalendar(self.month.year, self.month.month_number))
+
+    @property
+    def number_of_weeks(self) -> int:
+        """
+        Returns the number of weeks in the current month.
+
+        Returns:
+            An integer representing the number of weeks in the current month.
+        """
+        return len(calendar.monthcalendar(self.month.year, self.month.month_number))
+
+    @property
+    def number_of_days(self) -> int:
+        """
+        Returns the number of days in the current month.
+
+        Returns:
+            An integer representing the number of days in the current month.
+        """
+        return calendar.monthrange(self.month.year, self.month.month_number)[1]
+
+    @property
+    def first_week_day(self) -> str:
+        """
+        Returns the day of the week of the first day of the current month.
+
+        Returns:
+            A string representing the day of the week (e.g. "Monday").
+        """
+        return Week.days[datetime.date(self.month.year, self.month.month_number, 1).weekday()]
+
+
+    def list_of_weekday(self, weekday: str) -> list:
+        """
+        Returns a list of days in the current month that are given weekday.
+
+        Returns:
+            A list of integers representing the days of the month that are weekday.
+
+        Example:
+            If the current month is December 2022, the method would return [5, 12, 19, 26] for Mondays.
+        """
+        return [day for day in self.list_of_days if self.validator.is_day_in_weekday(day, weekday)]
+
+    def number_of_weekday(self, weekday: str) -> int:
+        """
+        Returns the number of weekday in the current month.
+
+        Returns:
+            An integer representing the number of weekday in the current month.
+        """
+        return len(self.list_of_weekday(weekday))
+
+    @property
+    def number_of_weekends(self) -> float:
+        """
+        Returns the number of weekend days (Saturdays and Sundays) in the current month.
+
+        The number of weekend days is calculated as the maximum number of Saturdays or Sundays in the month.
+
+        Returns:
+            An float representing the number of weekend days in the current month.
+        """
+        return (self.number_of_weekday("saturday") + self.number_of_weekday("sunday")) * 0.5
+
+
     def number_of_weekdays(self) -> int:
         """
         Returns the number of weekdays in the current month.
@@ -183,4 +171,41 @@ class DayOfWeek:
         Returns:
             The number of weekdays in the current month.
         """
-        return self.number_of_days() - (self.number_of_saturdays() + self.number_of_sundays())
+        return self.number_of_days - (self.number_of_weekday("saturday") + self.number_of_weekday("sundays"))
+
+
+
+@dataclass()
+class CurrentMonth:
+    year: int
+    month: int
+    validate: Validators
+    get: Retrievers
+
+    @property
+    def calendar(self):
+        """
+        Returns a matrix representing a month's calendar.
+
+        Each row represents a week, and each column represents a day of the Week.
+        Days that are not part of the current month are represented by zeros.
+
+        Returns:
+            A list of lists containing integers representing the days of the month.
+        """
+        return calendar.monthcalendar(self.year, self.month)
+
+
+def setfirstweekday(first_week_day: str):
+    Week.first_day = Week.inverted_days()[first_week_day.capitalize()]
+    calendar.setfirstweekday(Week.first_day)
+
+
+def new(year: int, month: int, first_week_day: str) -> CurrentMonth:
+    first_week_day = first_week_day
+    setfirstweekday(first_week_day.capitalize())
+    m = Month(year=year, month_number=month)
+    v = Validators(m)
+    g = Retrievers(m, v)
+    return CurrentMonth(year, month, v, g)
+
--- tests/test_month_info.py
+++ tests/test_month_info.py
@@ -4,76 +4,63 @@ import calendar
 from monthinfo import monthinfo
 
 
-class Test_WEEK(unittest.TestCase):
-    def test_setfirstweekda(self):
-        monthinfo.setfirstweekday("tuesday")
-        self.assertEqual(monthinfo.WEEK.first_day, 1)
-        monthinfo.setfirstweekday("friday")
-        self.assertEqual(monthinfo.WEEK.first_day, 4)
 
-class Test_DayOfWeek(unittest.TestCase):
+class Test_Week(unittest.TestCase):
+    def test_setfirstweekday(self):
+        monthinfo.setfirstweekday("tuesday")
+        self.assertEqual(monthinfo.Week.first_day, 1)
+        monthinfo.setfirstweekday("Friday")
+        self.assertEqual(monthinfo.Week.first_day, 4)
+
+
+class Test_CurrentMonth(unittest.TestCase):
     def setUp(self) -> None:
-        calendar = monthinfo.Month(2022, 12)
-        self.day_of_week = monthinfo.DayOfWeek(calendar)
+        self.calendar = monthinfo.new(2022, 12, "saturday")
+
+    def test_new(self):
+        self.assertEqual(self.calendar.month, 12)
 
     def test_first_week_day(self) -> str:
-        self.assertEqual(self.day_of_week.first_week_day, "Thursday")
+        self.assertEqual(self.calendar.get.first_week_day, "Thursday")
 
     def test_is_day_in_weekday(self):
-        self.assertTrue(self.day_of_week.is_day_in_weekday(3, "Saturday"))
-        self.assertFalse(self.day_of_week.is_day_in_weekday(5, "Saturday"))
+        self.assertTrue(self.calendar.validate.is_day_in_weekday(3, "Saturday"))
+        self.assertFalse(
+            self.calendar.validate.is_day_in_weekday(5, "Saturday"))
 
     def test_is_in_first_weekday(self):
-        self.assertTrue(self.day_of_week.is_in_first_weekday(3, "Saturday"))
+        self.assertTrue(self.calendar.validate.is_in_first_weekday(3, "Saturday"))
 
     def test_list_of_weekday(self):
-        self.assertEqual(self.day_of_week.list_of_weekday("saturday"), [3, 10, 17, 24, 31])
-
+        self.assertEqual(self.calendar.get.list_of_weekday(
+            "saturday"), [3, 10, 17, 24, 31])
 
     def test_number_of_weekday(self):
-        self.assertEqual(self.day_of_week.number_of_weekday("saturday"), 5)
-        self.assertEqual(self.day_of_week.number_of_weekday("monday"), 4)
+        self.assertEqual(self.calendar.get.number_of_weekday("saturday"), 5)
+        self.assertEqual(self.calendar.get.number_of_weekday("monday"), 4)
 
     def test_number_of_weekends(self):
-        self.assertEqual(self.day_of_week.number_of_weekends, 4.5)
+        self.assertEqual(self.calendar.get.number_of_weekends, 4.5)
 
 
     def test_is_weekend(self):
-        self.assertTrue(self.day_of_week.is_weekend(3))
-        self.assertFalse(self.day_of_week.is_weekend(2))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class Test_Month(unittest.TestCase):
-    def setUp(self) -> None:
-        monthinfo.setfirstweekday("saturday")
-        self.month = monthinfo.Month(2022, 12)
+        self.assertTrue(self.calendar.validate.is_weekend(3))
+        self.assertFalse(self.calendar.validate.is_weekend(2))
 
     def test_list_of_days(self):
-        self.assertEqual(self.month.list_of_days[0], 1)
-        self.assertEqual(self.month.list_of_days[29], 30)
+        self.assertEqual(self.calendar.get.list_of_days[0], 1)
+        self.assertEqual(self.calendar.get.list_of_days[29], 30)
 
     def test_list_of_weeks(self):
-        self.assertEqual(self.month.list_of_weeks[0], [0, 0, 0, 0, 0, 1, 2])
-        self.assertEqual(self.month.list_of_weeks[
+        self.assertEqual(self.calendar.get.list_of_weeks[0], [0, 0, 0, 0, 0, 1, 2])
+        self.assertEqual(self.calendar.get.list_of_weeks[
                          1], [3, 4, 5, 6, 7, 8, 9])
 
     def test_calendar(self):
-        self.assertEqual(self.month.calendar[0][6], 2)
+        self.assertEqual(self.calendar.calendar[0][6], 2)
 
     def test_number_of_days(self):
-        self.assertEqual(self.month.number_of_days, 31)
+        self.assertEqual(self.calendar.get.number_of_days, 31)
 
     def test_number_of_weeks(self):
-        self.assertEqual(self.month.number_of_weeks, 6)
+        self.assertEqual(self.calendar.get.number_of_weeks, 6)
